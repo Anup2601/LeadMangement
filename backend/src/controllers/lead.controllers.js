040d04d6db45de8a8984c8d63d7bddb3c23f1829
princesharma-2210 <shaarmprince@gmail.com>
2025-08-24 21:24:56 +0530

    pagination in server side

--- backend/src/controllers/lead.controllers.js
+++ backend/src/controllers/lead.controllers.js
@@ -13,14 +13,46 @@ export const createLead = async (req, res) => {
 };
 
 // Get all Leads
+// export const getLeads = async (req, res) => {
+//   try {
+//     const leads = await Lead.find();
+//     res.json(leads);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
+
+// Get Leads with Pagination
 export const getLeads = async (req, res) => {
   try {
-    const leads = await Lead.find();
-    res.json(leads);
+    // read query params
+    let { page = 1, limit = 20 } = req.query;
+
+    // convert to integers
+    page = parseInt(page, 10);
+    limit = Math.min(parseInt(limit, 10), 100); // max 100
+
+    const skip = (page - 1) * limit;
+
+    // get leads and total count
+    const [leads, total] = await Promise.all([
+      Lead.find().skip(skip).limit(limit),
+      Lead.countDocuments()
+    ]);
+
+    // response format
+    res.json({
+      data: leads,
+      page,
+      limit,
+      total,
+      totalPages: Math.ceil(total / limit),
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
 
 // Get Lead by ID
 export const getLeadById = async (req, res) => {
